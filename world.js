4ac2dfdfd39dabc7c9a21f9a3b229a7c5e6f611f
IsPrism <162273639+IsPrism@users.noreply.github.com>
2024-04-16 08:33:45 -0600

    Update world.js

--- world.js
+++ world.js
@@ -4,6 +4,8 @@ const ctx = canvas.getContext("2d");
 ctx.fillStyle = "green";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-const draw = () => {
-    ctx.drawImage()
-}
+const tileset = new Image();
+tileset.src = "tileset.png"
+tileset.onload = function() {
+    ctx.drawImage(tileset, 0, 0)
+};
