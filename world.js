e6d4f9204a96f5471e10f5887d4af23f44bed645
IsPrism <162273639+IsPrism@users.noreply.github.com>
2024-04-17 19:29:57 -0600

    Update resources folder (include more sprites)

--- world.js
+++ world.js
@@ -4,8 +4,8 @@ const ctx = canvas.getContext("2d");
 ctx.fillStyle = "green";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-const tileWidth = 32;
-const tileHeight = 32;
+const tileWidth = 16;
+const tileHeight = 16;
 const tileColumns = Math.round(canvas.width/tileWidth);
 const tileRows = Math.round(canvas.width/tileHeight);
 
@@ -17,11 +17,11 @@ const chunks = [
 
 function drawChunk() {
     ctx.drawImage(tileset, 0, 0, tileWidth, tileHeight, 0, 0, tileWidth, tileHeight);
-    ctx.drawImage(tileset, 32, 0, tileWidth, tileHeight, 32, 0, tileWidth, tileHeight);
+    ctx.drawImage(tileset, 16, 0, tileWidth, tileHeight, 16, 0, tileWidth, tileHeight);
+    ctx.drawImage(tileset, 0, 0, tileWidth, tileHeight, 32, 0, tileWidth, tileHeight);
+    ctx.drawImage(tileset, 16, 0, tileWidth, tileHeight, 48, 0, tileWidth, tileHeight);
     ctx.drawImage(tileset, 0, 0, tileWidth, tileHeight, 64, 0, tileWidth, tileHeight);
-    ctx.drawImage(tileset, 32, 0, tileWidth, tileHeight, 96, 0, tileWidth, tileHeight);
-    ctx.drawImage(tileset, 0, 0, tileWidth, tileHeight, 128, 0, tileWidth, tileHeight);
-    ctx.drawImage(tileset, 32, 0, tileWidth, tileHeight, 160, 0, tileWidth, tileHeight);
+    ctx.drawImage(tileset, 16, 0, tileWidth, tileHeight, 80, 0, tileWidth, tileHeight);
 };
 
 const tileset = new Image();
