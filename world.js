c6230c6bceb33258fd6b16d2ebb01900e2072061
IsPrism <162273639+IsPrism@users.noreply.github.com>
2024-04-23 19:29:21 -0600

    Update chunk rendering

--- world.js
+++ world.js
@@ -4,8 +4,8 @@ const ctx = canvas.getContext("2d");
 ctx.fillStyle = "green";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-const tileWidth = 40;
-const tileHeight = 40;
+const tileWidth = 70;
+const tileHeight = 70;
 const cropWidth = 16;
 const cropHeight = 16;
 
