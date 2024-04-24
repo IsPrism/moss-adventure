43857c48d6451ed9afd242f594ec043bab78886c
IsPrism <162273639+IsPrism@users.noreply.github.com>
2024-04-23 19:13:18 -0600

    Update world.js

--- world.js
+++ world.js
@@ -6,11 +6,6 @@ ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 const tileWidth = 16;
 const tileHeight = 16;
-const tileColumns = Math.round(canvas.width/tileWidth);
-const tileRows = Math.round(canvas.width/tileHeight);
 
 const tileset = new Image();
 tileset.src = "resources/tileset.png"
-
-console.log(tileColumns)
-console.log(tileRows)
