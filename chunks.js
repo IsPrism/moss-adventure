5557b09cd51cf4f7c77c50b00247fad49af86322
Erik <162273639+IsPrism@users.noreply.github.com>
2024-04-24 15:39:39 -0600

    Update chunks.js

--- chunks.js
+++ chunks.js
@@ -29,20 +29,20 @@ class Chunk {
     };
 };
 
-let chunk1 = new Chunk([0, 1, 0, 1, 0, 1, 0, 1, 0], 0, 0);
-let chunk2 = new Chunk([1, 0, 1, 0, 1, 0, 1, 0, 1], 1, 0);
-let chunk3 = new Chunk([0, 1, 0, 1, 0, 1, 0, 1, 0], 2, 0);
-let chunk4 = new Chunk([1, 0, 1, 0, 1, 0, 1, 0, 1], 3, 0);
+var chunk1 = new Chunk([0, 1, 0, 1, 0, 1, 0, 1, 0], 0, 0);
+var chunk2 = new Chunk([1, 0, 1, 0, 1, 0, 1, 0, 1], 1, 0);
+var chunk3 = new Chunk([0, 1, 0, 1, 0, 1, 0, 1, 0], 2, 0);
+var chunk4 = new Chunk([1, 0, 1, 0, 1, 0, 1, 0, 1], 3, 0);
 
-let chunk5 = new Chunk([1, 0, 1, 0, 1, 0, 1, 0, 1], 0, 1);
-let chunk6 = new Chunk([0, 1, 0, 1, 0, 1, 0, 1, 0], 1, 1);
-let chunk7 = new Chunk([1, 0, 1, 0, 1, 0, 1, 0, 1], 2, 1);
-let chunk8 = new Chunk([0, 1, 0, 1, 0, 1, 0, 1, 0], 3, 1);
+var chunk5 = new Chunk([1, 0, 1, 0, 1, 0, 1, 0, 1], 0, 1);
+var chunk6 = new Chunk([0, 1, 0, 1, 0, 1, 0, 1, 0], 1, 1);
+var chunk7 = new Chunk([1, 0, 1, 0, 1, 0, 1, 0, 1], 2, 1);
+var chunk8 = new Chunk([0, 1, 0, 1, 0, 1, 0, 1, 0], 3, 1);
 
-let chunk9 = new Chunk([0, 1, 0, 1, 0, 1, 0, 1, 0], 0, 2);
-let chunk10= new Chunk([1, 0, 1, 0, 1, 0, 1, 0, 1], 1, 2);
-let chunk11= new Chunk([0, 1, 0, 1, 0, 1, 0, 1, 0], 2, 2);
-let chunk12= new Chunk([1, 0, 1, 0, 1, 0, 1, 0, 1], 3, 2);
+var chunk9 = new Chunk([0, 1, 0, 1, 0, 1, 0, 1, 0], 0, 2);
+var chunk10= new Chunk([1, 0, 1, 0, 1, 0, 1, 0, 1], 1, 2);
+var chunk11= new Chunk([0, 1, 0, 1, 0, 1, 0, 1, 0], 2, 2);
+var chunk12= new Chunk([1, 0, 1, 0, 1, 0, 1, 0, 1], 3, 2);
 
 chunk1.drawChunk();
 chunk2.drawChunk();
