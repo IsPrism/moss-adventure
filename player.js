899981a759fd9bbfcfa9778666613d3c19e310e5
Erik <162273639+IsPrism@users.noreply.github.com>
2024-04-25 12:45:15 -0600

    Update player.js

--- player.js
+++ player.js
@@ -1,6 +1,6 @@
 class Player {
     constructor() {
-        ctx.drawImage("resources/player.png", 0, 0, 16, 16, 16, 16, 16, 16)
+        ctx.drawImage(playerImg, 0, 0, 16, 16, 16, 16, 16, 16)
     };
 };
 
