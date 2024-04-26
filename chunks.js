a1104bdb21dd523498acb070000132d0276b5370
Erik <162273639+IsPrism@users.noreply.github.com>
2024-04-26 13:48:26 -0600

    Update chunk rendering

--- chunks.js
+++ chunks.js
@@ -47,18 +47,3 @@ class Chunk {
         this.drawTile(8, 2, 2);
     };
 };
-
-var chunk1 = new Chunk([0, 1, 0, 1, 0, 1, 0, 1, 0], 0, 0);
-var chunk2 = new Chunk([1, 0, 1, 0, 1, 0, 1, 0, 1], 1, 0);
-var chunk3 = new Chunk([0, 1, 0, 1, 0, 1, 0, 1, 0], 2, 0);
-var chunk4 = new Chunk([1, 0, 1, 0, 1, 0, 1, 0, 1], 3, 0);
-
-var chunk5 = new Chunk([1, 0, 1, 0, 1, 0, 1, 0, 1], 0, 1);
-var chunk6 = new Chunk([0, 1, 0, 1, 0, 1, 0, 1, 0], 1, 1);
-var chunk7 = new Chunk([1, 0, 1, 0, 1, 0, 1, 0, 1], 2, 1);
-var chunk8 = new Chunk([0, 1, 0, 1, 0, 1, 0, 1, 0], 3, 1);
-
-var chunk9 = new Chunk([0, 1, 0, 1, 0, 1, 0, 1, 0], 0, 2);
-var chunk10= new Chunk([1, 0, 1, 0, 1, 0, 1, 0, 1], 1, 2);
-var chunk11= new Chunk([0, 1, 0, 1, 0, 1, 0, 1, 0], 2, 2);
-var chunk12= new Chunk([1, 0, 1, 0, 1, 0, 1, 0, 1], 3, 2);
--- world.js
+++ world.js
@@ -14,6 +14,22 @@ const cropHeight = 16;
 const tileset = new Image();
 
 tileset.onload = function() {
+    
+var chunk1 = new Chunk([0, 1, 0, 1, 0, 1, 0, 1, 0], 0, 0);
+var chunk2 = new Chunk([1, 0, 1, 0, 1, 0, 1, 0, 1], 1, 0);
+var chunk3 = new Chunk([0, 1, 0, 1, 0, 1, 0, 1, 0], 2, 0);
+var chunk4 = new Chunk([1, 0, 1, 0, 1, 0, 1, 0, 1], 3, 0);
+
+var chunk5 = new Chunk([1, 0, 1, 0, 1, 0, 1, 0, 1], 0, 1);
+var chunk6 = new Chunk([0, 1, 0, 1, 0, 1, 0, 1, 0], 1, 1);
+var chunk7 = new Chunk([1, 0, 1, 0, 1, 0, 1, 0, 1], 2, 1);
+var chunk8 = new Chunk([0, 1, 0, 1, 0, 1, 0, 1, 0], 3, 1);
+
+var chunk9 = new Chunk([0, 1, 0, 1, 0, 1, 0, 1, 0], 0, 2);
+var chunk10= new Chunk([1, 0, 1, 0, 1, 0, 1, 0, 1], 1, 2);
+var chunk11= new Chunk([0, 1, 0, 1, 0, 1, 0, 1, 0], 2, 2);
+var chunk12= new Chunk([1, 0, 1, 0, 1, 0, 1, 0, 1], 3, 2);
+
     drawAllChunks();
 };
 
@@ -23,10 +39,10 @@ window.requestAnimationFrame(gameLoop)
 
 
 function gameLoop() {
-    draw();
+    update();
     window.requestAnimationFrame(gameLoop)
 };
 
-function draw() {
+function update() {
     console.log("eenor")
 };
